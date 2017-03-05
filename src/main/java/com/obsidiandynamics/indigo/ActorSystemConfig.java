6f38bd138928730821682e36624b71fc4c514195
Emil Koutanov <ekoutanov@gmail.com>
2017-03-05 20:16:07 +0000

    Comments.

--- src/main/java/com/obsidiandynamics/indigo/ActorSystemConfig.java
+++ src/main/java/com/obsidiandynamics/indigo/ActorSystemConfig.java
@@ -1,12 +1,17 @@
 package com.obsidiandynamics.indigo;
 
 public abstract class ActorSystemConfig {
+  /** The number of threads for the dispatcher pool. This number is a guide only; the actual pool may
+   *  be sized dynamically. */
   protected int numThreads = getDefaultThreads();
   
-  protected long backlogCapacity = PropertyUtils.get("indigo.backlogCapacity", Long::parseLong, 10_000L);
+  /** The total (system-wide) backlog at which point throttling is enforced. */
+  protected long backlogCapacity = PropertyUtils.get("indigo.backlogCapacity", Long::parseLong, 100_000L);
   
+  /** The time penalty for each throttling block. */
   protected int backlogThrottleMillis = PropertyUtils.get("indigo.backlogThrottleMillis", Integer::parseInt, 1);
   
+  /** Upper bound on the number of consecutive blocks imposed during throttling. */
   protected int backlogThrottleTries = PropertyUtils.get("indigo.backlogThrottleTries", Integer::parseInt, 10);
   
   protected ActorConfig defaultActorConfig = new ActorConfig() {};
