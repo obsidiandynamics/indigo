c1eebc752abacab00b6a468fa2afdffd6a090282
Emil Koutanov <ekoutanov@gmail.com>
2017-03-24 00:17:28 +0000

    Updated HelloWorldSample.

--- src/test/java/com/obsidiandynamics/indigo/HelloWorldSample.java
+++ src/test/java/com/obsidiandynamics/indigo/HelloWorldSample.java
@@ -4,7 +4,7 @@ public final class HelloWorldSample {
   public static void main(String[] args) {
     new ActorSystemConfig() {}
     .define()
-    .when("echo").lambda(a -> System.out.println("Received " + a.message()))
+    .when("echo").lambda(a -> System.out.println(a.self() + " received " + a.message().body()))
     .ingress(a -> a.to(ActorRef.of("echo")).tell("hello world"))
     .shutdown();
   }
