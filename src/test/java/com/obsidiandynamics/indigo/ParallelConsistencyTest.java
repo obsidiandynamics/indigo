76bcd4b5b006afd79b145d0d6dff5eef0c8cf1b8
Emil Koutanov <ekoutanov@gmail.com>
2017-04-07 07:09:58 +0000

    Fast fan-in tests.

--- src/test/java/com/obsidiandynamics/indigo/ParallelConsistencyTest.java
+++ src/test/java/com/obsidiandynamics/indigo/ParallelConsistencyTest.java
@@ -11,46 +11,55 @@ public final class ParallelConsistencyTest implements TestSupport {
   private static final String SINK = "sink";
   private static final String DONE = "done";
 
-//  @Test
-//  public void testStraight() {
-//    test(1, 10, 1);
-//    test(10, 100, 1);
-//    test(100, 1_000, 1);
-//    test(10, 10_000, 1);
-//  }
+  @Test
+  public void testStraightUnbiased() {
+    test(1, 10, 1, 1);
+    test(10, 100, 1, 1);
+    test(100, 1_000, 1, 1);
+    test(10, 10_000, 1, 1);
+  }
   
   @Test
-  public void testFanIn() {
-//    test(1, 10, 10);
-//    test(10, 100, 100);
-    test(100, 1000, 100);
-//    test(10, 10_000, 10);
+  public void testStraightBiased() {
+    test(1, 10, 1, 1_000);
+    test(10, 100, 1, 1_000);
+    test(100, 1_000, 1, 1_000);
+    test(10, 10_000, 1, 1_000);
+  }
+  
+  @Test
+  public void testFanInBiased() {
+    test(1, 10, 10, 1_000);
+    test(10, 100, 100, 1_000);
+    test(100, 1_000, 100, 1_000);
+    test(10, 10_000, 10, 1_000);
   }
 
-  private void test(int actors, int runs, int fanIn) {
+  private void test(int actors, int runs, int fanIn, int sinkBias) {
     logTestName();
     
     final Set<ActorRef> doneRuns = new HashSet<>();
 
-    new TestActorSystemConfig() {{ 
-      defaultActorConfig = new ActorConfig() {{
-        bias = 1;
-        backlogThrottleCapacity = 10000;
-        backlogThrottleMillis = 1;
-        backlogThrottleTries = 1;
-      }};
-    }}
+    new TestActorSystemConfig() {}
     .define()
-    .when(DRIVER).lambda((a, m) -> {
+    .when(DRIVER).configure(new ActorConfig() {{
+      bias = 1;
+    }})
+    .lambda((a, m) -> {
       final ActorRef target = ActorRef.of(SINK, String.valueOf(Integer.valueOf(a.self().key()) % actors));
       for (int j = 1; j <= runs; j++) {
         a.to(target).tell(j);
       }
       a.to(ActorRef.of(DONE)).tell();
     })
-    .when(SINK).lambdaSync(() -> new int[fanIn], (a, m, s) -> {
+    .when(SINK).configure(new ActorConfig() {{
+      bias = sinkBias;
+      backlogThrottleCapacity = Integer.MAX_VALUE;
+      backlogThrottleMillis = 1;
+      backlogThrottleTries = 1;
+    }})
+    .lambdaSync(() -> new int[fanIn], (a, m, s) -> {
       final int msg = m.body();
-      if (msg % 100 == 0) System.out.println("threads=" + Thread.activeCount());
       log("got %d from %s\n", msg, m.from());
       final int actor = Integer.valueOf(m.from().key()) / actors;
       assertEquals(s[actor] + 1, msg);
