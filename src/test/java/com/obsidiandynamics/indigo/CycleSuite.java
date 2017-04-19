70e1c5e7ee6f42579e83d9b59c958410ab3cb7b2
Emil Koutanov <ekoutanov@gmail.com>
2017-04-19 00:17:01 +0000

    CycleSuite works.

--- src/test/java/com/obsidiandynamics/indigo/CycleSuite.java
+++ src/test/java/com/obsidiandynamics/indigo/CycleSuite.java
@@ -1,6 +1,8 @@
 package com.obsidiandynamics.indigo;
 
+import java.lang.annotation.*;
 import java.util.*;
+import java.util.stream.*;
 
 import org.junit.runner.*;
 import org.junit.runner.notification.*;
@@ -8,24 +10,110 @@ import org.junit.runners.*;
 import org.junit.runners.model.*;
 
 public final class CycleSuite extends Suite {
+  @Retention(RetentionPolicy.RUNTIME)
+  @Target(ElementType.TYPE)
+  @Inherited
+  public @interface ParameterMatrix {
+    public String[] keys();
+    public ParameterValues[] values();
+  }
+  
+  @Retention(RetentionPolicy.RUNTIME)
+  @Target(ElementType.ANNOTATION_TYPE)
+  @Inherited
+  public @interface ParameterValues {
+    public String[] value();
+  }
+  
+  private static final class Parameter {
+    private final String key;
+    private final String value;
+    
+    Parameter(String key, String value) {
+      this.key = key;
+      this.value = value;
+    }
+
+    void install() {
+      System.setProperty(key, value);
+    }
+  }
+  
+  private static final class ParametrisedRunner extends Runner {
+    private final Runner backing;
+    final Parameter[] params;
+    
+    ParametrisedRunner(Runner backing, Parameter[] params) {
+      this.backing = backing;
+      this.params = params;
+    }
+
+    @Override
+    public Description getDescription() {
+      return backing.getDescription();
+    }
+
+    @Override
+    public void run(RunNotifier notifier) {
+      backing.run(notifier);
+    }
+  }
+  
+  private final Parameter[][] matrix;
+  
   public CycleSuite(Class<?> klass, RunnerBuilder builder) throws InitializationError {
     super(klass, builder);
+    matrix = extractParams(klass.getAnnotation(ParameterMatrix.class));
+  }
+  
+  private static Parameter[][] extractParams(ParameterMatrix annotation) {
+    final Parameter[][] matrix = new Parameter[annotation.values().length][annotation.keys().length];
+    int i = 0;
+    for (ParameterValues values : annotation.values()) {
+      int j = 0;
+      for (String key : annotation.keys()) {
+        matrix[i][j] = new Parameter(key, values.value()[j]);
+        j++;
+      }
+      i++;
+    }
+    return matrix;
   }
 
   @Override
   protected List<Runner> getChildren() {
-    final List<Runner> runners = new ArrayList<>();
-    runners.addAll(super.getChildren());
-    runners.addAll(super.getChildren());
-    for (Runner runner : runners) {
-      System.out.println(runner.getDescription());
+    final List<Runner> children = super.getChildren();
+    final List<Runner> runners = new ArrayList<>(children.size() * matrix.length);
+    for (Parameter[] params : matrix) {
+      for (Runner child : children) {
+        try {
+          final Runner r = new BlockJUnit4ClassRunner(((BlockJUnit4ClassRunner) child).getTestClass().getJavaClass()) {
+            @Override protected Description describeChild(FrameworkMethod method) {
+              final Description s = super.describeChild(method);
+              return Description.createTestDescription(s.getClassName(), 
+                                                       s.getMethodName() + paramsToString(params), 
+                                                       s.getAnnotations().toArray(new Annotation[0]));
+            }
+
+            private String paramsToString(Parameter[] params) {
+              return Arrays.asList(params).stream().map(p -> p.value).collect(Collectors.toList()).toString();
+            }
+          };
+          runners.add(new ParametrisedRunner(r, params));
+        } catch (InitializationError e) {
+          e.printStackTrace();
+        } 
+      }
     }
     return runners;
   }
-
+  
   @Override
   protected void runChild(Runner runner, final RunNotifier notifier) {
+    final ParametrisedRunner cycleRunner = (ParametrisedRunner) runner; 
+    for (Parameter param : cycleRunner.params) {
+      param.install();
+    }
     runner.run(notifier);
-//    runner.run(notifier);
   }
 }
--- src/test/java/com/obsidiandynamics/indigo/PrimaryTests.java
+++ src/test/java/com/obsidiandynamics/indigo/PrimaryTests.java
@@ -1,29 +1,36 @@
 package com.obsidiandynamics.indigo;
 
 import org.junit.runner.*;
-import org.junit.runners.*;
 import org.junit.runners.Suite.*;
 
+import com.obsidiandynamics.indigo.CycleSuite.*;
 import com.obsidiandynamics.indigo.benchmark.*;
 
 @RunWith(CycleSuite.class)
 @SuiteClasses({
   DLQTest.class,
   DrainTest.class,
-  EchoBenchmark.class,
-  EgressTest.class,
-  ExceptionHandlerTest.class,
-  ExternalAskTest.class,
-  FaultTest.class,
-  ParallelConsistencyTest.class,
-  RequestResponseBenchmark.class,
-  RequestResponseTest.class,
-  StashTest.class,
-  StatelessChainTest.class,
-  StatelessLifeCycleTest.class,
-  StatefulLifeCycleTest.class,
-  ThrottleTest.class,
-  ThroughputBenchmark.class,
-  TimeoutTest.class
-})         
+    EchoBenchmark.class,
+    EgressTest.class,
+    ExceptionHandlerTest.class,
+    ExternalAskTest.class,
+    FaultTest.class,
+    ParallelConsistencyTest.class,
+    RequestResponseBenchmark.class,
+    RequestResponseTest.class,
+    StashTest.class,
+    StatelessChainTest.class,
+    StatelessLifeCycleTest.class,
+    StatefulLifeCycleTest.class,
+    ThrottleTest.class,
+    ThroughputBenchmark.class,
+    TimeoutTest.class
+})     
+@ParameterMatrix(
+  keys={ActorSystemConfig.Key.EXECUTOR,ActorConfig.Key.ACTIVATION_FACTORY},
+  values={@ParameterValues({"FIXED_THREAD_POOL","SYNC_QUEUE"}),
+          @ParameterValues({"FIXED_THREAD_POOL","NODE_QUEUE"}),
+          @ParameterValues({"FORK_JOIN_POOL","SYNC_QUEUE"}),
+          @ParameterValues({"FORK_JOIN_POOL","NODE_QUEUE"})
+})
 public class PrimaryTests {}
