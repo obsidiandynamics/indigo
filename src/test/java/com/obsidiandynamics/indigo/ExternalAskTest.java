854df31168033f09bb9fea102afdcf0c97c10982
Emil Koutanov <ekoutanov@gmail.com>
2017-04-19 02:28:45 +0000

    Added test for ask() cancellation.

--- src/test/java/com/obsidiandynamics/indigo/ExternalAskTest.java
+++ src/test/java/com/obsidiandynamics/indigo/ExternalAskTest.java
@@ -1,10 +1,13 @@
 package com.obsidiandynamics.indigo;
 
-import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.*;
 
 import java.util.concurrent.*;
+import java.util.concurrent.atomic.*;
 
 import org.junit.*;
+
+import com.obsidiandynamics.indigo.util.*;
 
 public final class ExternalAskTest implements TestSupport {
   private static final String ADDER = "adder";
@@ -26,6 +29,37 @@ public final class ExternalAskTest implements TestSupport {
     }
   }
 
+  @Test
+  public void testCancel() throws InterruptedException, ExecutionException, TimeoutException {
+    logTestName();
+    
+    final AtomicBoolean ran = new AtomicBoolean();
+    final CyclicBarrier barrier = new CyclicBarrier(2);
+    
+    final ActorSystem system = new TestActorSystemConfig() {}
+    .define()
+    .when(ADDER).lambda((a, m) -> {
+      a.reply(m).tell(m.<Integer>body() + 1);
+      ran.set(true);
+    });
+    
+    // block the ingress lambda using a barrier, preventing further message delivery
+    system.ingress(a -> {
+      Threads.await(barrier);
+    });
+    
+    final CompletableFuture<Integer> f = system.ask(ActorRef.of(ADDER), 41);
+    f.cancel(false);
+    
+    // unblock the ingress lambda; it should not deliver the cancelled message to the adder
+    Threads.await(barrier);
+    
+    system.drain(0);
+    
+    assertFalse(ran.get());
+    system.shutdownQuietly();
+  }
+
   @Test(expected=TimeoutException.class)
   public void testTimeoutWithCancel() throws InterruptedException, ExecutionException, TimeoutException {
     logTestName();
@@ -34,7 +68,7 @@ public final class ExternalAskTest implements TestSupport {
     .define()
     .when(ADDER).lambda((a, m) -> { /* do nothing, stalling the reply */ });
     
-    final CompletableFuture<Integer> f = system.ask(ActorRef.of(ADDER), 1_000, 41);
+    final CompletableFuture<Integer> f = system.ask(ActorRef.of(ADDER));
     try {
       f.get(10, TimeUnit.MILLISECONDS);
     } finally {
@@ -51,7 +85,7 @@ public final class ExternalAskTest implements TestSupport {
     .define()
     .when(ADDER).lambda((a, m) -> { /* do nothing, stalling the reply */ });
     
-    final CompletableFuture<Integer> f = system.ask(ActorRef.of(ADDER), 1_000, 41);
+    final CompletableFuture<Integer> f = system.ask(ActorRef.of(ADDER), 41);
     try {
       f.get(10, TimeUnit.MILLISECONDS);
     } finally {
