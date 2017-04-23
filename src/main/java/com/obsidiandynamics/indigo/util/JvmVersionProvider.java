bf1fe933e6eb50dde23fae05e1abd99a6e4b916c
Emil Koutanov <ekoutanov@gmail.com>
2017-04-23 00:21:44 +0000

    JvmVersionProvider no longer based on a singleton pattern.

--- src/main/java/com/obsidiandynamics/indigo/util/JvmVersionProvider.java
+++ src/main/java/com/obsidiandynamics/indigo/util/JvmVersionProvider.java
@@ -1,28 +1,18 @@
 package com.obsidiandynamics.indigo.util;
 
-public abstract class JvmVersionProvider {
-  private static JvmVersionProvider PROVIDER = new DefaultJvmVersionProvider();
-
+@FunctionalInterface
+public interface JvmVersionProvider {
   public static final class JvmVersion {
     public final int major;
     public final int minor;
     public final int update;
     public final int build;
 
-    private JvmVersion(int major, int minor, int update, int build) {
+    public JvmVersion(int major, int minor, int update, int build) {
       this.major = major;
       this.minor = minor;
       this.update = update;
       this.build = build;
-    }
-    
-    public static JvmVersion parse(String versionStr) {
-      final String[] versionFrags = versionStr.split("\\.|_");
-      final int major = Integer.parseInt(versionFrags[0]);
-      final int minor = Integer.parseInt(versionFrags[1]);
-      final int update = Integer.parseInt(versionFrags[2]);
-      final int build = Integer.parseInt(versionFrags[3]);
-      return new JvmVersion(major, minor, update, build);
     }
 
     @Override
@@ -31,28 +21,38 @@ public abstract class JvmVersionProvider {
     }
   }
 
-  public static final class DefaultJvmVersionProvider extends JvmVersionProvider {
+  public static final class DefaultProvider implements JvmVersionProvider {
+    private final String versionStr;
+    
+    public DefaultProvider() {
+      this(System.getProperty("java.version"));
+    }
+    
+    public DefaultProvider(String versionStr) {
+      this.versionStr = versionStr;
+    }
+    
     @Override
     public JvmVersion get() {
-      final String versionStr = System.getProperty("java.version");
       try {
-        return JvmVersion.parse(versionStr);
+        return parse(versionStr);
       } catch (Exception e) {
         final String fallbackStr = "1.8.0_1";
-        System.err.format("WARNING: cannot parse system property 'java.version' with value '%s'; falling back to '%s'\n", 
+        System.err.format("WARNING: cannot parse version string '%s'; falling back to '%s'\n", 
                           versionStr, fallbackStr);
-        return JvmVersion.parse(fallbackStr);
+        return parse(fallbackStr);
       }
+    }
+    
+    private static JvmVersion parse(String versionStr) {
+      final String[] versionFrags = versionStr.split("\\.|_");
+      final int major = Integer.parseInt(versionFrags[0]);
+      final int minor = Integer.parseInt(versionFrags[1]);
+      final int update = Integer.parseInt(versionFrags[2]);
+      final int build = Integer.parseInt(versionFrags[3]);
+      return new JvmVersion(major, minor, update, build);
     }
   }
 
-  public static final void setProvider(JvmVersionProvider provider) {
-    PROVIDER = provider;
-  }
-
-  public static final JvmVersion getVersion() {
-    return PROVIDER.get();
-  }
-
-  public abstract JvmVersion get();
+  JvmVersion get();
 }
