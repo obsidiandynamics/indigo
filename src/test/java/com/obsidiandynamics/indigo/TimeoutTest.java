78fd9ea7764d74341958f4a33e86b5e610a06a1f
Emil Koutanov <ekoutanov@gmail.com>
2017-04-24 08:40:41 +0000

    Increased default timeout tolerance.

--- src/test/java/com/obsidiandynamics/indigo/TimeoutTest.java
+++ src/test/java/com/obsidiandynamics/indigo/TimeoutTest.java
@@ -55,7 +55,7 @@ public final class TimeoutTest implements TestSupport {
     }
     final double avgDiff = (double) totalDiff / actors;
     
-    final int timeoutTolerance = get(KEY_TIMEOUT_TOLERANCE, Integer::parseInt, 10);
+    final int timeoutTolerance = get(KEY_TIMEOUT_TOLERANCE, Integer::parseInt, 50);
     log("Average diff: %.1f\n", avgDiff);
     assertTrue(String.format("Average timeout threshold above tolerance: %.1f", avgDiff), 
                avgDiff <= timeoutTolerance);
