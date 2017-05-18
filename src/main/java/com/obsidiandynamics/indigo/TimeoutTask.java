bb9fa23a74fe5ba24c7a0396811fe93a0ba0233b
Emil Koutanov <ekoutanov@gmail.com>
2017-05-18 06:28:04 +0000

    Temporary instrumentation for RejectedExecutionException added to the TimeoutTask. Remove when appropriate.

--- src/main/java/com/obsidiandynamics/indigo/TimeoutTask.java
+++ src/main/java/com/obsidiandynamics/indigo/TimeoutTask.java
@@ -1,6 +1,7 @@
 package com.obsidiandynamics.indigo;
 
 import java.util.*;
+import java.util.concurrent.*;
 
 import com.obsidiandynamics.indigo.task.*;
 
@@ -10,17 +11,26 @@ final class TimeoutTask extends Task<UUID> {
   private final PendingRequest request;
   
   private final Endpoint endpoint;
+  
+  private final StackTraceElement[] stack; //TODO remove
 
   TimeoutTask(long expiresAt, UUID requestId, ActorRef actorRef, PendingRequest request, Endpoint endpoint) {
     super(expiresAt, requestId);
     this.actorRef = actorRef;
     this.request = request;
     this.endpoint = endpoint;
+    stack = Thread.currentThread().getStackTrace();
   }
   
   @Override
   protected void execute() {
-    endpoint.send(new Message(null, actorRef, Timeout.instance(), getId(), true));
+    try {
+      endpoint.send(new Message(null, actorRef, Timeout.instance(), getId(), true));
+    } catch (RejectedExecutionException e) {
+      System.err.println("TimeoutTask.execute");
+      System.err.println(Arrays.asList(stack).toString().replace(',', '\n'));
+      throw e;
+    }
   }
   
   @Override
