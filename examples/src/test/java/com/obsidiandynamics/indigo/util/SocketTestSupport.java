a61dc625d1187c561196716f17da5fe12e2c1b4d
Emil Koutanov <ekoutanov@gmail.com>
2017-06-13 03:23:17 +0000

    Alternate algo for testing socket availability.

--- examples/src/test/java/com/obsidiandynamics/indigo/util/SocketTestSupport.java
+++ examples/src/test/java/com/obsidiandynamics/indigo/util/SocketTestSupport.java
@@ -41,29 +41,20 @@ public interface SocketTestSupport {
     }
     throw new NoAvailablePortsException("No available ports in the range " + preferredPort + " - " + MAX_PORT);
   }
-  
-  static boolean isPortAvailable(int port) {
-    ServerSocket ss = null;
-    DatagramSocket ds = null;
-    try {
-      ss = new ServerSocket(port);
-      ss.setReuseAddress(true);
-      ds = new DatagramSocket(port);
-      ds.setReuseAddress(true);
-      return true;
-    } catch (IOException e) {
-    } finally {
-      if (ds != null) {
-        ds.close();
-      }
 
-      if (ss != null) {
+  static boolean isPortAvailable(int port) {
+    Socket s = null;
+    try {
+      s = new Socket("localhost", port);
+      return false;
+    } catch (IOException e) {
+      return true;
+    } finally {
+      if( s != null){
         try {
-          ss.close();
+          s.close();
         } catch (IOException e) {}
       }
     }
-
-    return false;
   }
 }
