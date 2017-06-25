9563f006ab89d15eed7bcdb50e775cdaf1f2ad04
Emil Koutanov <ekoutanov@gmail.com>
2017-06-26 09:10:40 +1000

    Updated sample.

--- examples/src/main/java/com/obsidiandynamics/indigo/hello/HelloWorldSample.java
+++ examples/src/main/java/com/obsidiandynamics/indigo/hello/HelloWorldSample.java
@@ -5,8 +5,8 @@ import com.obsidiandynamics.indigo.*;
 public final class HelloWorldSample {
   public static void main(String[] args) throws InterruptedException {
     ActorSystem.create()
-    .on("echo").cue((a, m) -> System.out.println(a.self() + " received " + m.body()))
-    .ingress(a -> a.to(ActorRef.of("echo")).tell("hello world"))
+    .on("sysout").cue((a, m) -> System.out.println(a.self() + " received " + m.body()))
+    .ingress(a -> a.to(ActorRef.of("sysout")).tell("hello world"))
     .shutdown();
   }
 }
